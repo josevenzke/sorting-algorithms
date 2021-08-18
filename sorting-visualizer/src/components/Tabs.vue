350958cca01c3742580915503e3812268a4ff6c4
josevenzke <josevenzkejr@gmail.com>
2021-08-17 23:27:27 -0300

    new component for tabs

---
+++ sorting-visualizer/src/components/Tabs.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tabs-container">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Bubble Sort</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Insertion Sort</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Selection Sort</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Tabs',
+}
+</script>
+
+<style scoped>
+.tabs-container{
+    width: 900px;
+    margin:auto;
+}
+.active{
+    background-color: #ff8fa3 !important;
+    color: #001219 !important;
+}
+.nav-link{
+    border: 0px !important;
+}
+</style>
