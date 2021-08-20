224ea5683e92cbbd87de36173fe1bd7a4b04962a
josevenzke <josevenzkejr@gmail.com>
2021-08-20 00:10:44 -0300

    removed unused functions, and added new tabs

--- sorting-visualizer/src/App.vue
+++ sorting-visualizer/src/App.vue
@@ -8,8 +8,16 @@
       <button @click="fillArray()">Shuffle</button>
     </div>
     <div class="container">
-      <Tabs />
-      <BubbleSort />
+      <Tabs @changeTab="changeTab" />
+      <div class="tab-body" v-if="activeTab=='bubble'">
+        <BubbleSort />
+      </div>
+      <div class="tab-body" v-if="activeTab=='insertion'">
+        <InsertionSort />
+      </div>
+      <div class="tab-body" v-if="activeTab=='selection'">
+        <SelectionSort />
+      </div>
     </div>
   </div>
 </template>
@@ -17,107 +25,30 @@
 <script>
 import Tabs from './components/Tabs.vue'
 import BubbleSort from './components/SortingAlgs/BubbleSort.vue'
+import InsertionSort from './components/SortingAlgs/InsertionSort.vue'
+import SelectionSort from './components/SortingAlgs/SelectionSort.vue'
 
 export default {
   name: 'App',
   components:{
     Tabs,
-    BubbleSort
+    BubbleSort,
+    InsertionSort,
+    SelectionSort
   },
   data() {
     return {
       array: [],
       numArray: [],
       size: 40,
+      activeTab: 'bubble',
     }
   },
   methods:{
-    fillArray(){
-      this.array = [];
-      for (let i =0; i< this.size; i++) {
-        this.array.push([this.getRndInteger(5, 400),'grey'])
-        this.numArray.push(this.array[i][0])
-      }
-      console.log(this.numArray)
+    changeTab(tab){
+      this.activeTab  = tab
     },
-    getRndInteger(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) ) + min;
-    },
-    async bubbleSort(){
-      let len = this.array.length
-      let checked;
-      do {
-        checked = false;
-        for(let i =0;i<len-1;i++){
-          if(this.array[i][0] > this.array[i+1][0]){
-            let tmp = this.array[i][0];
-            
-            this.array[i][0] = this.array[i+1][0];
-            this.array[i+1][0] = tmp;
-            this.array[i+1][1] = 'green';
-            // sleep - to visualize / see the changes
-            await this.sleep(25)
-            this.array[i+1][1] = 'grey'
-            checked = true
-          }
-        }
-      } while(checked)
-      this.paintArray('lightseagreen')
-    },
-    async insertionSort(){
-      for(let i = 1; i < this.size; i++){
-        let key = this.array[i][0];
-        let j = i - 1;
-          
-        //Sort in the ascending order
-        while(j >= 0 && this.array[j][0] > key){
-          this.array[j+1][1] = 'green'
-          this.array[j][1] = 'green'
-          await this.sleep(10)
-          this.array[j + 1][0] = this.array[j][0];
-          this.array[j+1][1] = 'lightseagreen'
-          this.array[j][1] = 'lightseagreen'
-          j = j - 1;
-        }
-
-        this.array[j + 1][0] = key;
-      }
-    },
-    async selectionSort(){
-      for(var i = 0; i < this.size; i++){
-        var min = i; 
-        for(var j = i+1; j < this.size; j++){
-          if(this.array[j][0] < this.array[min][0]){
-          // update the smallest index
-            min = j; 
-          }
-        }
-        var tmp = this.array[i][0];
-        this.array[i][1] = 'green'
-        this.array[min][1] = 'red'
-        await this.sleep(300)
-        this.array[i][0] = this.array[min][0];
-        this.array[min][0] = tmp;
-        this.array[i][1] = 'red'
-        this.array[min][1] = 'green'
-        await this.sleep(300)
-        this.array[i][1] = 'grey'
-        this.array[min][1] = 'grey'
-      }
-      this.paintArray('lightseagreen')
-    },
-    sleep(time) {
-      return new Promise((resolve) => setTimeout(resolve, time));
-    },
-    paintArray(color){
-      for(var i = 0;i<this.size;i++){
-          this.array[i][1] = color
-      }
-    }
   },
-  created() {
-    this.fillArray();
-  }
 }
 </script>
 
@@ -130,13 +61,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.bar-container {
-  margin: auto;
-  background-color: #ff8fa3;
-  border-radius: 0px 0px 15px 15px;
-  padding: 20px;
-  width: 900px;
-}
+
 .buttons-container{
   margin: 10px;
 }
@@ -153,5 +78,10 @@ export default {
 .container{
   padding: 30px;
   background-color: #fff0f3;
+}
+.tab-body{
+  background-color: #ff8fa3;
+  width:900px;
+  margin: 0px 90px 0px 90px;
 }
 </style>
