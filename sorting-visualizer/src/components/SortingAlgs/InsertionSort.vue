664c545ed66967362b32a52b9afc1a09ae74aede
josevenzke <josevenzkejr@gmail.com>
2021-08-20 00:13:26 -0300

    created insertionSort component

---
+++ sorting-visualizer/src/components/SortingAlgs/InsertionSort.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <button @click="insertionSort()">Sort</button>
+        <Bars :numArray="array" :key="array"/>
+    </div>
+</template>
+
+<script>
+import Bars from '../Bars.vue'
+import arrayMixin from '../../mixins/arrayMixin'
+
+export default {
+    name:'InsertionSort',
+    components:{
+        Bars
+    },
+    mixins: [arrayMixin],
+    data(){
+        return{
+            array: [],
+            size: 40,
+        }
+    },
+    mounted() {
+        this.fillArray();
+    },
+    methods:{
+        async insertionSort(){
+            for(let i = 1; i < this.size; i++){
+                let key = this.array[i][0];
+                let j = i - 1;
+            
+                //Sort in the ascending order
+                while(j >= 0 && this.array[j][0] > key){
+                this.array[j+1][1] = 'green'
+                this.array[j][1] = 'green'
+                await this.sleep(10)
+                this.array[j + 1][0] = this.array[j][0];
+                this.array[j+1][1] = 'lightseagreen'
+                this.array[j][1] = 'lightseagreen'
+                j = j - 1;
+                }
+                this.array[j + 1][0] = key;
+            }
+        },
+    },
+}
+</script>
