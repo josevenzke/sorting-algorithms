b76ff93915438d1e728afb59aa0e385690a9592e
josevenzke <josevenzkejr@gmail.com>
2021-08-20 00:11:57 -0300

    now working properly

--- sorting-visualizer/src/components/SortingAlgs/BubbleSort.vue
+++ sorting-visualizer/src/components/SortingAlgs/BubbleSort.vue
@@ -1,58 +1,54 @@
 <template>
     <div>
         <button @click="bubbleSort()">Sort</button>
-        <Bars :numArray="array" :key="array" />
+        <Bars :numArray="array" :key="array"/>
     </div>
 </template>
 
 <script>
 import Bars from '../Bars.vue'
+import arrayMixin from '../../mixins/arrayMixin'
+
 export default {
     name:'BubbleSort',
     components:{
         Bars
     },
+    mixins: [arrayMixin],
     data(){
         return{
             array: [],
+            size: 40,
         }
     },
-    created() {
+    mounted() {
         this.fillArray();
     },
     methods:{
-        fillArray(){
-            this.array = [];
-            for (let i =0; i< this.size; i++) {
-                this.array.push([this.getRndInteger(5, 400),'grey'])
-            }
-        },
-        getRndInteger(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) ) + min;
-        },
         async bubbleSort(){
             let len = this.array.length
+            var last = this.size-1
             let checked;
             do {
                 checked = false;
                 for(let i =0;i<len-1;i++){
-                if(this.array[i][0] > this.array[i+1][0]){
-                    let tmp = this.array[i][0];
-                    
-                    this.array[i][0] = this.array[i+1][0];
-                    this.array[i+1][0] = tmp;
-                    this.array[i+1][1] = 'green';
-                    // sleep - to visualize / see the changes
-                    await this.sleep(25)
-                    this.array[i+1][1] = 'grey'
-                    checked = true
+                    if(this.array[i][0] > this.array[i+1][0]){
+                        let tmp = this.array[i][0];
+                        
+                        this.array[i][0] = this.array[i+1][0];
+                        this.array[i+1][0] = tmp;
+                        this.array[i+1][1] = 'green';
+                        // sleep - to visualize / see the changes
+                        await this.sleep(25)
+                        this.array[i+1][1] = 'grey'
+                        checked = true
+                        
+                    }
                 }
-                }
+                this.array[last][1] = 'lightseagreen'
+                last = last-1
             } while(checked)
             this.paintArray('lightseagreen')
-        },
-        sleep(time) {
-            return new Promise((resolve) => setTimeout(resolve, time));
         },
     },
 }
