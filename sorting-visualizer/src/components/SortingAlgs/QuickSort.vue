d91299482874706622f90de329aa51f77cacf9f1
josevenzke <josevenzkejr@gmail.com>
2021-08-21 05:54:55 -0300

    quicksort component now fully working

--- sorting-visualizer/src/components/SortingAlgs/QuickSort.vue
+++ sorting-visualizer/src/components/SortingAlgs/QuickSort.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button @click="quickSort(array)">Sort</button>
+        <button @click="quickSortWrap(array,0,array.length-1)">Sort</button>
         <Bars :numArray="array" :key="array"/>
     </div>
 </template>
@@ -19,50 +19,43 @@ export default {
         return{
             array: [],
             size: 90,
+            sortedArray: []
         }
     },
     created() {
         this.fillArray()
+        for (let i = 0; i < this.size; i++) {
+            this.sortedArray.push(this.array[i][0])
+        }
+        this.sortedArray = this.sortedArray.sort(function(a, b) {return a - b;})
+        console.log(this.sortedArray)
+        console.log(this.array)
     },
     methods:{
-        async quickSort() {
-            // Creating an array that we'll use as a stack, using the push() and pop() functions
-            var stack = [];
-            
-            // Adding the entire initial array as an "unsorted subarray"
-            stack.push(0);
-            stack.push(this.array.length - 1);
-            
-            // There isn't an explicit peek() function
-            // The loop repeats as long as we have unsorted subarrays
-            while(stack[stack.length - 1] >= 0){
-                
-                // Extracting the top unsorted subarray
-                var end = stack.pop();
-                var start = stack.pop();
-                
-                var pivotIndex = await this.partition(this.array, start, end);
-                
-                // If there are unsorted elements to the "left" of the pivot,
-                // we add that subarray to the stack so we can sort it later
-                if (pivotIndex - 1 > start){
-                    stack.push(start);
-                    stack.push(pivotIndex - 1);
-                }
-                
-                // If there are unsorted elements to the "right" of the pivot,
-                // we add that subarray to the stack so we can sort it later
-                if (pivotIndex + 1 < end){
-                    stack.push(pivotIndex + 1);
-                    stack.push(end);
-                    }
-                await this.sleep(100)
-                console.log('a')
-                }
+        async quickSortWrap(arr,start,end){
+            await this.quickSort(arr,start,end)
             this.paintArray('lightseagreen')
+        },
+        async quickSort(arr,start,end) {
+            if (start >= end) {
+                return;
+            }
+            
+            // Returns pivotIndex
+            let index = await this.partition(arr, start, end);
+            
+            // Recursively apply the same logic to the left and right subarrays
+            await Promise.all([this.quickSort(arr, start, index - 1),this.quickSort(arr, index + 1, end)])
+            for (let i = 0; i < this.size; i++) {
+                if (arr[i][0]==this.sortedArray[i]){
+                    arr[i][1] = 'lightseagreen'
+                }
+            }
+            await this.sleep(200);
         },
         async partition(arr, start, end){
             // Taking the last element as the pivot
+
             const pivotValue = arr[end][0];
             let pivotIndex = start; 
             for (let i = start; i < end; i++) {
@@ -73,7 +66,15 @@ export default {
                 pivotIndex++;
                 }
             }
-            
+            for (let i = start; i < end; i++) {
+                arr[i][1] = 'green'
+            }
+
+            await this.sleep(200);
+            for (let i = start; i < end; i++) {
+                arr[i][1] = 'grey'
+            }
+
             // Putting the pivot value in the middle
             [arr[pivotIndex][0], arr[end][0]] = [arr[end][0], arr[pivotIndex][0]] 
             return pivotIndex;
