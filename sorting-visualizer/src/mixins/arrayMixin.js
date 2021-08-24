5376e72bd7bfb9b87eb38b5cd2935cd4f27114e7
josevenzke <josevenzkejr@gmail.com>
2021-08-24 01:28:59 -0300

    created shuffle function, and added size to data

--- sorting-visualizer/src/mixins/arrayMixin.js
+++ sorting-visualizer/src/mixins/arrayMixin.js
@@ -1,10 +1,25 @@
 export default {
+  data(){
+    return{
+      size: 200,
+    }
+  },
   methods: {
     fillArray(){
       this.array = [];
-      for (let i =0; i< this.size; i++) {
-          this.array.push([this.getRndInteger(5, 400),'grey'])
+      for (let i =1; i< this.size+1; i++) {
+          this.array.push([i*2,'grey'])
       }
+    },
+    shuffle(array) {
+      var j, x, i;
+      for (i = array.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          x = array[i];
+          array[i] = array[j];
+          array[j] = x;
+      }
+      this.paintArray('grey')
     },
     getRndInteger(min, max) {
       return Math.floor(Math.random() * (max - min + 1) ) + min;
